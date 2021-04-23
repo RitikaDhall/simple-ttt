9c07b1128efe00e8711506025b1acfcdd94a8988
Ritika Dhall <ritikapdhall@gmail.com>
2021-04-23 22:21:02 +0530

    Part 3 done

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Board extends JFrame {
     private Container pane;
@@ -8,19 +10,60 @@ public class Board extends JFrame {
     private boolean hasWinner;
     private JMenuBar menuBar;
     private JMenu menu;
-    private JMenuItem quit;
     private JMenuItem newGame;
+    private JMenuItem quit;
 
     public Board() {
-
+        super();
+        pane = getContentPane();
+        pane.setLayout(new GridLayout(3, 3));
+        setTitle("Tic Tac Toe");
+        setSize(500, 500);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        currentPlayer = "x";
+        board = new JButton[3][3];
+        hasWinner = false;
+        initializeBoard();
+        initializeMenuBar();
     }
 
     private void initializeMenuBar() {
-        
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+
+        newGame = new JMenuItem("New game");
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetBoard();
+            }            
+        });
+
+        quit = new JMenuItem("Quit");
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }            
+        });
+
+        menu.add(newGame);
+        menu.add(quit);
+
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
     }
 
     private void resetBoard() {
-        
+        currentPlayer = "x";
+        hasWinner = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j].setText("");
+            }
+        }
     }
 
     private void initializeBoard() {
@@ -32,6 +75,21 @@ public class Board extends JFrame {
     }
 
     private void hasWinner() {
-        
+        // To do: complete all winning combos
+        // 1st col
+        if(board[0][0].getText().equals(currentPlayer) && board[1][0].getText().equals(currentPlayer) && board[2][0].getText().equals(currentPlayer)) {
+            JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!");
+            hasWinner = true;
+        }
+        // 2nd col
+        else if(board[0][1].getText().equals(currentPlayer) && board[1][1].getText().equals(currentPlayer) && board[2][1].getText().equals(currentPlayer)) {
+            JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!");
+            hasWinner = true;
+        }
+        // 3rd col
+        else if(board[0][2].getText().equals(currentPlayer) && board[1][2].getText().equals(currentPlayer) && board[2][2].getText().equals(currentPlayer)) {
+            JOptionPane.showMessageDialog(null, "Player " + currentPlayer + " wins!");
+            hasWinner = true;
+        }
     }
 }
